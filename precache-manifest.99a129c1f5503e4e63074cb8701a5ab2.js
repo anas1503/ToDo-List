0c52e4cf94547ef9abb1e2b8b0d8806f2284778b
anas1503 <md.anas1503@gmail.com>
2020-09-29 11:57:43 +0530

    Updates

---
+++ precache-manifest.99a129c1f5503e4e63074cb8701a5ab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bbb4d0bde7c15f093f9d7130e01693",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "f0f975d78c1ee6f9964d",
+    "url": "/ToDo-List/static/css/main.fff8bb8f.chunk.css"
+  },
+  {
+    "revision": "80ee7228494aa6be8de5",
+    "url": "/ToDo-List/static/js/2.4fdba6eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-List/static/js/2.4fdba6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f975d78c1ee6f9964d",
+    "url": "/ToDo-List/static/js/main.da8c600f.chunk.js"
+  },
+  {
+    "revision": "9ba9b2f5c513f3539cb0",
+    "url": "/ToDo-List/static/js/runtime-main.808f6706.js"
+  }
+]);
